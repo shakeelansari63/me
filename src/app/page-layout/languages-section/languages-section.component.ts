0e7473897e3deb21d8c2024c8779a89d3d47c517
Shakeel Ansari <ShakeelAnsari63@Gmail.com>
2024-05-12 15:06:33 +0530

    Show languages

--- src/app/page-layout/languages-section/languages-section.component.ts
+++ src/app/page-layout/languages-section/languages-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { GithubStatsService } from '../../services/github-stats.service';
 
 @Component({
   selector: 'app-languages-section',
   templateUrl: './languages-section.component.html',
-  styleUrl: './languages-section.component.scss'
+  styleUrl: './languages-section.component.scss',
 })
 export class LanguagesSectionComponent {
-
+  constructor(public githubStats: GithubStatsService) {}
 }
--- src/app/services/github-stats.service.ts
+++ src/app/services/github-stats.service.ts
@@ -22,4 +22,16 @@ export class GithubStatsService {
   public getUserGithubStars() {
     return `${this.githubStatsbaseUrl}/api?username=${userData.githubUser}&show_icons=true&include_all_commits=true&theme=${this.theme}&hide_border=true&show=reviews,discussions_started,discussions_answered,prs_merged,prs_merged_percentage&hide=stars,commits,prs,issues,contribs`;
   }
+
+  public getTopLanguageByRepo() {
+    return `${this.profileStatsBaseUrl}/api/cards/repos-per-language?username=${userData.githubUser}&theme=${this.theme}`;
+  }
+
+  public getTopLanguageByCommit() {
+    return `${this.profileStatsBaseUrl}/api/cards/most-commit-language?username=${userData.githubUser}&theme=${this.theme}`;
+  }
+
+  public getTopLanguages() {
+    return `${this.githubStatsbaseUrl}/api/top-langs/?username=${userData.githubUser}&layout=compact&theme=${this.theme}&hide_border=true&&langs_count=8`;
+  }
 }
