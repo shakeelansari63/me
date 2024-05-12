1ccd8da304fbc2d1b17101d43c2dfe730234a367
Shakeel Ansari <ShakeelAnsari63@Gmail.com>
2024-05-12 13:05:23 +0530

    use theme var

--- src/app/services/github-stats.service.ts
+++ src/app/services/github-stats.service.ts
@@ -7,8 +7,9 @@ import { userData } from '../data/user-data';
 export class GithubStatsService {
   constructor() {}
   private baseApiUrl = 'http://github-profile-summary-cards.vercel.app';
+  private theme = 'dark';
 
   public getUserMainStats() {
-    return `${this.baseApiUrl}/api/cards/profile-details?username=${userData.githubUser}&theme=dark`;
+    return `${this.baseApiUrl}/api/cards/profile-details?username=${userData.githubUser}&theme=${this.theme}`;
   }
 }
